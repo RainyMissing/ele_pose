a2aa1387c3c78b3d6223774cde4de3928f8aac45
tomas <rainhotcold@gmail.com>
2024-07-29 16:24:13 +0800

    Update README, env_yolo.yaml, and train.py with new changes

--- train.py
+++ train.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     model = YOLO('yolov8n-pose.yaml').load('yolov8n-pose.pt')  # you can change the model here, e.g., use 'yolov8x-pose'
     # Example:
     # model = YOLO('yolov8s-pose.yaml').load('yolov8s-pose.pt')
-    results = model.train(data='./data/configuration.yaml', epochs=4000, imgsz=640, batch=12, patience=0, pose=25)  # Set dataset path. If there is a path error, change the path to an absolute path
+    results = model.train(data='./data/configuration.yaml', epochs=4000, imgsz=640, batch=12, patience=0, pose=25)  # Set dataset path. If there is a path error, change the path to an absolute path, like "D:/ele_proj/proj_5/data/configuration.yaml"
